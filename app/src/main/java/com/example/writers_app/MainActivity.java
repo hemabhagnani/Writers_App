81ef08f4e193654c8472edb9a7f7cfc109ee842c
Hema bhagnani <hemabhagnani5@gmail.com>
2020-10-29 13:23:25 +0530

    test 2

--- app/src/main/java/com/example/writers_app/MainActivity.java
+++ app/src/main/java/com/example/writers_app/MainActivity.java
@@ -1,28 +1,74 @@
 package com.example.writers_app;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.icu.text.CaseMap;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView listView;
+    String mTitle[] ={"hema","amit","dipisha","muskan","riya"};
+    String mDescription[]={"gfgtr","gfgtr","gfgtr","gfgtr","gfgtr"};
+int image[]={R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,R.drawable.ic_launcher_background};
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView=findViewById(R.id.listview);
+
+        MyAdapter adapter=new MyAdapter(this,mTitle,mDescription,image);
+        listView.setAdapter(adapter);
     }
 
-    public void doSomething(View view) {
-        Intent i=new Intent(this,second.class);
-        String[] names = {"hema","amit","riya","muskan","dipisha"};
-        Random random=new Random();
-        int pos=random.nextInt(names.length);
-        i.putExtra("key",names[pos]);
-        startActivity(i);
+    class MyAdapter extends ArrayAdapter<String>{
+        Context context;
+                String rTitle[];
+        String rDescription[];
+        int rImage[];
 
+        MyAdapter(Context c,String title[],String description[],int image[]){
+            super(c,R.layout.row,R.id.textview1,title);
+            this.context=c;
+            this.rTitle=title;
+            this.rDescription=description;
+            this.rImage=image;
+        }
+
+        @NonNull
+        @Override
+
+        public  View getView(int position, @Nullable View convertView, @Nullable ViewGroup parent){
+            LayoutInflater layoutInflater=(LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View row=layoutInflater.inflate(R.layout.row,parent,false);
+            ImageView image=row.findViewById(R.id.image);
+            TextView myTitle=row.findViewById(R.id.textview1);
+            TextView mydescription=row.findViewById(R.id.textview2);
+
+            image.setImageResource(rImage[position]);
+            myTitle.setText(rTitle[position]);
+            mydescription.setText(rDescription[position]);
+
+            return row;
+
+        }
     }
+
+
 }
